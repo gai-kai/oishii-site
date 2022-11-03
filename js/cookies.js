1da958a58e9d3fd0fab024fe182ba17f6828e927
gai-kai <gaikai@mailbox.org>
2022-11-03 14:31:13 +0100

    fix cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,4 +1,4 @@
-import {BootstrapCookieConsentSettings} from "bootstrap-cookie-consent-settings";
+import {BootstrapCookieConsentSettings} from "./cookie-consent";
 this.props = {
     autoShowDialog: true, // disable autoShowModal on the privacy policy and legal notice pages, to make these pages readable
     lang: navigator.language, // the language, in which the modal is shown
