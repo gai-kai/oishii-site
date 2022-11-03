81fca19560c9acce830da1f12a87ff9e5d0a7c99
gai-kai <gaikai@mailbox.org>
2022-11-03 14:20:09 +0100

    fix checkIfStopped

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-
 var defaultDate
 const CUSTOMER="oishii"
 const BASE_URL = "https://api.gaikai.xyz/"+CUSTOMER;
@@ -44,37 +43,6 @@ const weekDayOpenObjects = [
     weekDayOpenFriday,
     weekDayOpenSaturday,
 ]
-
-import {BootstrapCookieConsentSettings} from "bootstrap-cookie-consent-settings"
-this.props = {
-    autoShowDialog: true, // disable autoShowModal on the privacy policy and legal notice pages, to make these pages readable
-    lang: navigator.language, // the language, in which the modal is shown
-    languages: [ "de"], // supported languages (in ./content/), defaults to first in array
-    contentURL: "../content", // this URL must contain the dialogs content in the needed languages
-    cookieName: "cookie-consent-settings",  // the name of the cookie in which the configuration is stored as JSON
-    cookieStorageDays: 365, // the duration the cookie configuration is stored on the client
-    postSelectionCallback: getScript // callback function, called after the user has made his selection
-}
-var cookieSettings = new BootstrapCookieConsentSettings(props)
- cookieSettings.showDialog();
-
-function getScript() {
-    var cookies = cookieSettings.getSettings();
-    if(cookies.analyses){
-        let script = document.createElement( 'script' );
-        script.type = 'text/javascript';
-        script.src = "https://www.googletagmanager.com/gtag/js?id=G-6YMRPBDDN1";
-        let script2 = document.createElement( 'script' );
-        script2.innerHTML="  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-6YMRPBDDN1');"
-        document.head.appendChild(script);
-        document.head.appendChild(script2);
-
-        console.log('getScript executed');
-    }else  {
-        console.log('getScript not executed')
-    }
-
-}
 
 window.addEventListener('load', () => {
     const now = new Date();
@@ -130,8 +98,6 @@ function wantNewsletter(){
 
 
 }
-checkIfStopped()
-loadDefaultValuesReservation()
 
 function displayInvalidTime() {
     document.getElementById("invalidTimeSign").style.display = "block"
@@ -317,7 +283,8 @@ function showWarnText(){
     document.getElementById("invalidInformation").style.display = 'block'
 }
 
-
+checkIfStopped()
+loadDefaultValuesReservation()
 window.addEventListener("cookieAlertAccept", function() {
     alert("cookies accepted")
 })
